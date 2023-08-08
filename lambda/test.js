85354c8da4ccbb088dff5f644f31a64e474ba35b
anubhav.jhalani@nordcloud.com <anubhav.jhalani@nordcloud.com>
2023-08-08 10:24:09 +0200

    updated lambda

--- lambda/test.js
+++ lambda/test.js
@@ -1,5 +1,24 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: "db-instance-identifier.cht3qw2hlkr0.us-east-1.rds.amazonaws.com",
+    user: "admin",
+    password: "Lumia640!",
+    database: "databasename",
+});
+
 export const handler = async (event) => {
-  // TODO implement
+
+  connection.query('show tables', function (error, results, fields) {
+    if (error) {
+        connection.destroy();
+        throw error;
+    } else {
+        // connected!
+        console.log(results);
+        callback(error, results);
+        connection.end(function (err) { callback(err, results);});
+    }
+}); 
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from github!!'),
