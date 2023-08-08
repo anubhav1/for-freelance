200dd273fc9a89f124b412994f0d63c626215992
anubhav.jhalani@nordcloud.com <anubhav.jhalani@nordcloud.com>
2023-08-08 11:24:37 +0200

    node_modules added

--- lambda/test.js
+++ lambda/test.js
@@ -1,27 +1,17 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host: "db-instance-identifier.cht3qw2hlkr0.us-east-1.rds.amazonaws.com",
+    host: "db-instance-identifier-snapshot.cht3qw2hlkr0.us-east-1.rds.amazonaws.com",
     user: "admin",
     password: "Lumia640!",
     database: "databasename",
 });
 
-export const handler = async (event) => {
-
-  connection.query('show tables', function (error, results, fields) {
-    if (error) {
-        connection.destroy();
-        throw error;
-    } else {
-        // connected!
-        console.log(results);
-        callback(error, results);
-        connection.end(function (err) { callback(err, results);});
-    }
-}); 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from github!!'),
-  };
-  return response;
+exports.handler = async (event) => {
+  const sql = "CREATE TABLE MESSAGE (message VARCHAR(255))";
+  connection.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+  return "Table Created"
 };
+
